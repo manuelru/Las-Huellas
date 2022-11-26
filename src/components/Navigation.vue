e793607953bbdc2a5002337e12933fe7866e9947
Manuel <manuelruiz26042000@gmail.com>
2022-11-26 09:32:25 -0600

    agregar mas vistas

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,22 +1,54 @@
 <template>
-    <nav class="navegacio">
-     
-                <router-link  class="rutas" to="/"> Home </router-link>
-                <router-link  class="rutas" to="/products">Productos</router-link>
-    </nav>
+    <section class="sectionNavegacion">
+        <div class="divLogo">
+            <img class="logo" src="../../public/logoproyectofina.jpeg" alt="">
+            <p class="nameFarmacia">Las Huellas</p>
+
+        </div>
+        <nav class="navegacio">
+ 
+            <router-link  class="rutas" to="/"> Home </router-link>
+            <router-link  class="rutas" to="/products">Farmacia Veterinaria</router-link>
+            <router-link  class="rutas" to="/accesorios">Accesorios</router-link>
+            <router-link  class="rutas" to="/contactanos">Contactanos</router-link>
+            <router-link  class="rutas" to="/servicios">Servicios</router-link>
+        </nav>
+
+    </section>
+
 </template>
 <style>
+.logo{
+    margin-left: 20px;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+}
+.sectionNavegacion{
+    display: flex;
+}
+.divLogo{
+    width: 40%;
+    height: 60px;
+    display: flex;
+}
+.nameFarmacia{
+    margin-left: 10px;
+    font-weight: 600;
+    font-size: 20px;
+}
 .rutas{
     text-decoration: none;
-    margin-left: 15.5%;
+    margin-left: 10%;
     color: black;
-    font-size: 20px;
+    font-size: 15px;
 }
 img{
     width: 50px;
 }
 .navegacio{
-    padding-left: 50%;
-    height: 60px;
-    }
+    width: 60%;
+    height: 40px;
+    padding-top: 20px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,12 +6,18 @@ import Home from "./pages/Home.vue";
 import Products from "./pages/Products.vue";
 import Vitaminas from "./pages/Vitaminas.vue";
 import Desparasitantes from "./pages/Desparasitantes.vue";
+import Accesorios from "./pages/Accesorios.vue"
+import Contactanos from "./pages/Contactanos.vue"
+import Servicios from "./pages/Servicios.vue"
 
 const routes = [
   { path: "/", component: Home },
   { path: "/products", component: Products },
   { path: "/desparasitantes", component: Desparasitantes },
   { path: "/vitaminas", component: Vitaminas },
+  { path: "/accesorios", component: Accesorios },
+  { path: "/contactanos", component: Contactanos },
+  { path: "/servicios", component: Servicios },
 ];
 
 const router = createRouter({
