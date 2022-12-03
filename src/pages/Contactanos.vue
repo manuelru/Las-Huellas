f7b5c6a782fc8f4dba8d85bc346effeb7a599797
Manuel <manuelruiz26042000@gmail.com>
2022-12-02 22:17:01 -0600

    3

--- src/pages/Contactanos.vue
+++ src/pages/Contactanos.vue
@@ -6,20 +6,21 @@
     
     <h2>Contacto clinica veterinaria las Huellas</h2>
 
-    <Span><img class="icon" src="https://cdn-icons-png.flaticon.com/512/7382/7382751.png" alt="">Direccion: Zona #1 del monumento del mercado 75 varas al sur Nueva Guinea  </Span>
+    <Span><img class="icon" src="https://cdn-icons-png.flaticon.com/512/7382/7382751.png" alt=""> Direccion: Zona #1 del monumento del mercado 75 varas al sur Nueva Guinea  </Span>
     <br> <br>
     <p>
-        <span><img class="icon"  src="https://cdn-icons-png.flaticon.com/512/561/561253.png" alt=""></span>
-        Tels:  55-5360-3311 
+        <span> <img class="icon"  src="https://cdn-icons-png.flaticon.com/512/561/561253.png" alt="">Tels:  55-5360-3311</span>
+         
 
     </p>
     
     <p>
-        <span><img class="icon" src="https://as2.ftcdn.net/v2/jpg/01/81/64/67/1000_F_181646714_C2bO4TVNye0ObkR7X10dAdHjr4lKkW9m.jpg" alt=""></span>
-        Urgencias las 24  Horas</p>
+        <span><img class="icon" src="https://as2.ftcdn.net/v2/jpg/01/81/64/67/1000_F_181646714_C2bO4TVNye0ObkR7X10dAdHjr4lKkW9m.jpg" alt=""> Urgencias las 24  Horas</span>
+       
+    </p>
     <h3>Atencion a Clientes</h3>
     <p>
-        luiscalero097@gmail.compute <BR></BR>
+        luiscalero097@gmail.compute <br> <br>
         LUIS ALEJANDRO REYES CALERO
     </p>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31256.396350136576!2d-84.47263263044233!3d11.690901366499055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f0b6d79b80b8e59%3A0x85146f89424620a6!2sNueva%20Guinea!5e0!3m2!1ses!2sni!4v1670029800531!5m2!1ses!2sni" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
